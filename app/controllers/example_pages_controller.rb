2b55cdc4ebe37d00ff0ec41f968e17dbf9db3dfe
Colleen <colleenrust524@EMAIL.com>
2023-03-27 13:04:54 -0500

    add how are you doing route. add compliment route

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -2,4 +2,10 @@ class ExamplePagesController < ApplicationController
   def plants
     render json: {message:"hello"}
   end
+  def dogs
+    render json:{message:"how ya doing today?"}
+  end 
+  def compliment
+    render json:{message:"you look great!!!"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/how_are_ya_doing", controller: "example_pages", action: "dogs"
+
+
+  get "/appearance", controller: "example_pages", action: "compliment"
+
 end
